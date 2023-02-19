ebd2bb2bad66730dd703a7dcb49116982a7ba652
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-02-19 02:46:12 -0800

    0.1.6: WIP - copying middle links to layout2

--- pages/learn/nested/layout2.tsx
+++ pages/learn/nested/layout2.tsx
@@ -1,7 +1,7 @@
 import Header2 from "./header2"
 import type { ReactNode } from "react"
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default function Layout2({ children }: { children: ReactNode }) {
   return (
     <>
       <Header2 />
