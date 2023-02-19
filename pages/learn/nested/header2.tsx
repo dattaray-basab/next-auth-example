d312ad898037aaab475f12e50f1d4c8bed7485b9
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-02-19 02:53:31 -0800

    0.1.7: WIP - copying middle links to header2 - also made middle a folder with index

--- pages/learn/nested/header2.tsx
+++ pages/learn/nested/header2.tsx
@@ -11,20 +11,16 @@ export default function Header2() {
         <style>{`.nojs-show { opacity: 1; top: 0; }`}</style>
       </noscript>
 
-      <nav>
-        <ul className={styles.navItems}>
-          <li className={styles.navItem}>
-            <Link href='/'>Home</Link>
+      <div>
+        <ul>
+          <li>
+            <Link href='/learn/htm'>HTML...</Link>
           </li>
-          <li className={styles.navItem}>
-            <Link href='/learn'>The Atef Learning Site</Link>
+          <li>
+            <Link href='/learn/js'>Java Script...</Link>
           </li>
-          <li className={styles.navItem}>
-            <Link href='/ws'>Private Workspace</Link>
-          </li>
-    
         </ul>
-      </nav>
+      </div>
     </header>
   );
 }
