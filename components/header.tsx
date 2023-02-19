d1cea368b05b1c4ffd296796a0745b5d9a51f833
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-02-18 18:43:16 -0800

    0.0.7: START - started adding learn

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,13 @@
-import Link from "next/link"
-import { signIn, signOut, useSession } from "next-auth/react"
-import styles from "./header.module.css"
+import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
+import styles from "./header.module.css";
 
 // The approach used in this component shows how to build a sign in and sign out
 // component that works on pages which support both client and server side
 // rendering, and avoids any flash incorrect content on initial page load.
 export default function Header() {
-  const { data: session, status } = useSession()
-  const loading = status === "loading"
+  const { data: session, status } = useSession();
+  const loading = status === "loading";
 
   return (
     <header>
@@ -67,26 +67,29 @@ export default function Header() {
             <Link href='/'>Home</Link>
           </li>
           <li className={styles.navItem}>
-            <Link href='/learning'>learning</Link>
+            <Link href='/learn'>The Atef Learning Site</Link>
           </li>
           <li className={styles.navItem}>
+            <Link href='/ws'>Private Workspace</Link>
+          </li>
+          {/* <li className={styles.navItem}>
             <Link href='/client'>Client</Link>
-          </li>
-          <li className={styles.navItem}>
+          </li> */}
+          {/* <li className={styles.navItem}>
             <Link href='/server'>Server</Link>
-          </li>
-          <li className={styles.navItem}>
+          </li> */}
+          {/* <li className={styles.navItem}>
             <Link href='/protected'>Protected</Link>
-          </li>
-          <li className={styles.navItem}>
+          </li> */}
+          {/* <li className={styles.navItem}>
             <Link href='/api-example'>API</Link>
-          </li>
+          </li> */}
           <li className={styles.navItem}>
             <Link href='/admin'>Admin</Link>
           </li>
-          <li className={styles.navItem}>
+          {/* <li className={styles.navItem}>
             <Link href='/me'>Me</Link>
-          </li>
+          </li> */}
         </ul>
       </nav>
     </header>
