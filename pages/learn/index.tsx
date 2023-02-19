e2821fcf31adc4d99ad75872caa24e0c8020917d
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-02-19 02:14:29 -0800

    0.1.4: WIP - setting up for nested nav

--- pages/learn/index.tsx
+++ pages/learn/index.tsx
@@ -1,10 +1,23 @@
 import Layout from "../../components/layout";
+import Link from "next/link";
 
 export default function Learn() {
   return (
     <Layout>
       <h1> Learn...</h1>
+      <div>
+        <ul>
+          <li>
+            <Link href='/'>Home</Link>
+          </li>
+          <li>
+            <a>Sidenav link 2</a>
+          </li>
+          <li>
+            <a>Sidenav link 2</a>
+          </li>
+        </ul>
+      </div>
     </Layout>
   );
 }
-1.
