220b8821a9caba8db227d0a83b50e7876ecce626
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-02-19 20:30:37 -0800

    0.1.14: WIP - intro _nav

---
+++ pages/learn/_nav/header2.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import styles from "./header2.module.css";
+
+
+export default function Header2() {
+
+
+  return (
+    <header>
+      <noscript>
+        <style>{`.nojs-show { opacity: 1; top: 0; }`}</style>
+      </noscript>
+
+    </header>
+  );
+}
