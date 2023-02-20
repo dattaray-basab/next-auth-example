0dc52efb8f73598595b0cfee74766e702b9e09ca
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-02-19 20:06:32 -0800

    0.1.11: WIP - dinking with Js page

--- pages/learn/htm/index.jsx
+++ pages/learn/htm/index.jsx
@@ -4,7 +4,7 @@ import Layout2 from "../layout_format/layout2";
 export default function Htm() {
   return (
     <Layout2>
-        <Link href='/learn/htm/htm-content'>HTML...</Link>
+        <Link href='/learn/htm/htm-content'>HTML Content</Link>
     </Layout2>
   );
 }
--- pages/learn/js/index.jsx
+++ pages/learn/js/index.jsx
@@ -1,10 +1,10 @@
-
+import Link from "next/link";
 import Layout2 from "../layout_format/layout2";
 
 export default function Js() {
   return (
     <Layout2>
-        <div>Js...</div>
+      <Link href='/learn/js/js-content'>JS Content</Link>
     </Layout2>
   );
 }
