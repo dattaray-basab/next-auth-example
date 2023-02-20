c138604e6490d6b700c6cab0ae2e7e58df56d2f0
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-02-19 21:36:10 -0800

    0.1.17.2: WIP - added contents

--- pages/learn/js/index.jsx
+++ pages/learn/js/index.jsx
@@ -1,10 +1,15 @@
-import Link from "next/link";
-import Layout from "../_nav/layout";
-
-export default function Js() {
+const JSPage = () => {
   return (
-    <Layout>
-      
-    </Layout>
+    <section>
+      <div className='px-4 py-4 sm:px-6 lg:px-8'>
+        <h1 className='mb-3 text-2xl font-semibold'>Java Script</h1>
+        <div className='flex items-center gap-2'>
+          <span className='text-2xl'>👈🏼</span>
+          <span>js...</span>
+        </div>
+      </div>
+    </section>
   );
-}
+};
+
+export default JSPage;
