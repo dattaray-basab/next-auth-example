10911c5f4ae01f8dc651717863e0b8508dbc0d43
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-02-19 20:39:38 -0800

    0.1.15: WIP - using _nav convention

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Layout from "../components/layout"
+import Layout from "./_nav/layout"
 
 export default function IndexPage() {
   return (
--- pages/learn/index.tsx
+++ pages/learn/index.tsx
@@ -1,4 +1,4 @@
-import Layout from "../../components/layout";
+import Layout from "../_nav/layout";
 
 import Js from "./js";
 import Htm from "./htm";
