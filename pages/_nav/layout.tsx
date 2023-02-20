9a43f0b8eb8c4d2428e1b4ebfe7862b50e362b4e
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-02-20 02:52:57 -0800

    0.1.20: WIP - side nav not  working

--- pages/_nav/layout.tsx
+++ pages/_nav/layout.tsx
@@ -1,6 +1,7 @@
 import Header from "./header"
 import Footer from "./footer"
 import type { ReactNode } from "react"
+// import '../globals.css'
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
--- pages/learn/_nav/layout.tsx
+++ pages/learn/_nav/layout.tsx
@@ -1,6 +1,7 @@
 import Header from "./header";
 import type { ReactNode } from "react";
 
+
 export default function Layout2({ children }: { children: ReactNode }) {
   return (
     <>
